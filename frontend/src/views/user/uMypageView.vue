04713761d679c8d60d7ca65374ed8e6c295bd053
euna <somdays01@gmail.com>
2023-03-10 11:55:38 +0900

    euna0310commit

--- frontend/src/views/user/uMypageView.vue
+++ frontend/src/views/user/uMypageView.vue
@@ -1,130 +1,72 @@
 <template>
 
-  <div>
-    <div>
-      <span>채용제목</span>
-      <input type="text" placeholder="내용을 입력하세요.">
+  <div class="container" style="max-width:1560px;">
+  <!--이력서 관리-->
+    <div class="resumeBoxCon">
+      <div>
+        <h3>이력서 관리</h3>
+        <button class="arrDown"><img src="@/assets/icon_arr_down.png"></button>
+      </div>
+<!--      <div class="row">-->
+      <div class="resumeBoxWrap">
+        <div class="resumeBox">
+          <div class="left">
+            <p class="resumeTitle">창의적인 개발자가 되겠습니다.</p>
+            <p>2023.03.09</p>
+          </div>
+          <div class="right">
+            <button type="button" class="btn btn-outline-primary" style="border-color: #0064ff;color:#0064ff;">수정</button>
+            <button type="button" class="btn btn-outline-primary" style="border-color: rgb(229, 75, 75);color:rgb(229, 75, 75);">삭제</button>
+          </div>
+        </div>
+      </div>
+      <div class="resumeBoxWrap">
+      <div class="resumeBox">
+        <div class="left">
+          <p class="resumeTitle">창의적인 개발자가 되겠습니다.</p>
+          <p>2023.03.09</p>
+        </div>
+        <div class="right">
+          <button type="button" class="btn btn-outline-primary" style="border-color: #0064ff;color:#0064ff;">수정</button>
+          <button type="button" class="btn btn-outline-primary" style="border-color: rgb(229, 75, 75);color:rgb(229, 75, 75);">삭제</button>
+        </div>
+      </div>
+      </div>
+<!--      </div>-->
     </div>
 
-    <div>
-      <span>접수기간</span>
-      <span>2023.03.03~2023.06.05</span>
+
+    <hr>
+
+
+    <div class="aplBoxCon">
+      <div>
+        <h3>지원 현황</h3>
+      </div>
+      <div class="aplBtnBox">
+        <button>
+          <p class="aplBtnNum">30</p>
+          <p>전체</p>
+        </button>
+        <button>
+          <p class="aplBtnNum">30</p>
+          <p>지원완료</p>
+        </button>
+        <button>
+          <p class="aplBtnNum">30</p>
+          <p>서류통과</p>
+        </button>
+        <button>
+          <p class="aplBtnNum">30</p>
+          <p>최종합격</p>
+        </button>
+        <button>
+          <p class="aplBtnNum">30</p>
+          <p>불합격</p>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <span>인사담당자 정보입력란</span> <br/>
-      <span>채용조건</span>
-      <span>1. 포지션 정보</span>
-
-      <table>
-        <thead>
-        <tr>
-          <td>포지션</td>
-          <td><input type="text" placeholder="직무명을 포함하여 입력하세요."></td>
-
-
-          <td>고용형태</td>
-        </tr>
-        </thead>
-
-        <tbody>
-        <tr>
-          <td>직무</td>
-          <td><input type="text"><a href="#">전체보기</a></td>
-        </tr>
-
-        <tr>
-          <td>경력</td>
-          <td><input type="checkbox"></td>
-          <td>신입/경력/경력무관</td>
-        </tr>
-
-        <tr>
-          <td>고용형태</td>
-          <td><input type="checkbox"></td>
-          <td>정규직/계약직/인턴/파견직/아르바이트</td>
-        </tr>
-        </tbody>
-      </table>
-
-    </div>
-
-    <div>
-      <p>채용조건</p>
-      <form>
-        <table>
-          <tr>
-            <td>학력</td>
-            <td>
-              <select>
-                <option>대졸(4년제)</option>
-                <option>전문대졸(3년제)</option>
-                <option>전문대졸(2년제)</option>
-                <option>고졸</option>
-                <option>학력무관</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>우대조건</td>
-            <td>
-              <select>
-                <option>국가유공자</option>
-                <option>보훈대상자</option>
-                <option>고용촉진지원금 대상자</option>
-                <option>취업보호대상자</option>
-                <option>병역특례</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>자격증</td>
-            <td>
-              <select>
-                <option>정보처리기사</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>성별연령무관</td>
-            <td>
-              <input type="radio" name="gender"/>여자
-              <input type="radio" name="gender"/>남자
-            </td>
-          </tr>
-
-        </table>
-
-        <table>
-          <tr>
-            <td>급여</td>
-            <td>
-              <select>
-                <option>연봉</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>근무지주소</td>
-            <td>
-              <input type="text">
-            </td>
-          </tr>
-          <tr>
-            <td>근무일</td>
-            <td>
-              <input type="text">
-            </td>
-          </tr>
-          <tr>
-            <td>근무시간</td>
-            <td>
-              <input type="text">
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
 
 
   </div>
@@ -139,4 +81,59 @@ export default {
 
 <style scoped>
 
+.left{float: left;}
+.right {float:right;}
+/*이력서*/
+.resumeBoxCon h3 {display: inline-block;}
+
+.resumeBoxWrap { display: inline-block;width:50%;}
+
+.arrDown{border:0;
+      margin-left:10px;
+  background: transparent;
+}
+.arrDown img{border:0;
+          width:20px;
+  height:30px;
+  object-fit: contain;
+}
+
+.resumeBox {border: 1px solid #dedede;
+                border-radius: 20px;
+  overflow: hidden;
+  padding:20px;
+width:98%;
+  margin-bottom:10px;
+  display:inline-block;
+  margin-left:5px;
+    }
+.resumeBox p {margin:0;padding: 0;}
+.resumeBox button {margin:5px;}
+
+.resumeBox:hover {border:1px solid #0064ff;}
+    .resumeTitle {font-weight: bold;
+                  font-size: 18px;
+    }
+
+/*지원현황*/
+.aplBtnBox {margin-top:20px;}
+.aplBtnBox button {
+  border:0;
+  border-left:1px solid #dedede;
+  background-color: transparent;
+  width:100px;
+}
+
+.aplBtnBox button:hover {color:#0064ff;}
+.aplBtnBox button
+
+.aplBtnBox button:first-child {border:0;}
+
+.aplBtnNum {
+
+    font-size: 22px;
+    font-weight: bold;
+    margin:0;
+    padding:0;
+}
 </style>
